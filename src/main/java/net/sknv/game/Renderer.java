6285a73357fec69cd183911acb4ec9df3ccc089f
dffsa <danyfreitas100@hotmail.com>
2019-08-11 15:19:32 +0100

    unused import

--- src/main/java/net/sknv/game/Renderer.java
+++ src/main/java/net/sknv/game/Renderer.java
@@ -6,7 +6,6 @@ import net.sknv.engine.Utils;
 import net.sknv.engine.Window;
 import net.sknv.engine.graph.*;
 import org.joml.Matrix4f;
-import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
 
