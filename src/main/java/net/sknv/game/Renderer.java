9954058593437ba465a2b3920402b044369354cb
secknv <secknv@gmail.com>
2019-08-11 18:51:40 +0100

    Added light code to shaders

--- src/main/java/net/sknv/game/Renderer.java
+++ src/main/java/net/sknv/game/Renderer.java
@@ -28,8 +28,8 @@ public class Renderer {
     public void init(Window window) throws Exception {
         //create shader
         shaderProgram = new ShaderProgram();
-        shaderProgram.createVertexShader(Utils.loadResource("/shaders/vertex.vs"));
-        shaderProgram.createFragmentShader(Utils.loadResource("/shaders/fragment.fs"));
+        shaderProgram.createVertexShader(Utils.loadResource("/shaders/vertex.glsl"));
+        shaderProgram.createFragmentShader(Utils.loadResource("/shaders/fragment.glsl"));
         shaderProgram.link();
 
         //create uniforms for world and projection matrices
---
+++ src/main/resources/shaders/fragment.glsl
@@ -0,0 +1,86 @@
+#version 330
+
+in  vec2 outTexCoord;
+in vec3 mvVertexNormal;
+in vec3 mvVertexPos;
+
+out vec4 fragColor;
+
+struct Attenuation {
+    float constant;
+    float linear;
+    float exponent;
+};
+
+struct PointLight {
+    vec3 color;
+    //pos in view space coords
+    vec3 postition;
+    float intensity;
+    Attenuation att;
+};
+
+struct Material {
+    vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+    int hasTexture;
+    float reflectance;
+};
+
+uniform sampler2D texture_sampler;
+uniform vec3 ambientLight;
+uniform float specularPower;
+uniform Material material;
+uniform PointLight pointLight;
+
+vec4 ambientC;
+vec4 diffuseC;
+vec4 specularC;
+
+void setupColors(Material material, vec2 textCoord) {
+    if (material.hasTexture == 1) {
+        ambientC = texture(texture_sampler, textCoord);
+        diffuseC = ambientC;
+        specularC = ambientC;
+    }
+    else {
+        ambientC = material.ambient;
+        diffuseC = material.diffuse;
+        specularC = material.specular;
+    }
+}
+
+vec4 calcPointLight(PointLight light, vec3 postition, vec3 normal) {
+    vec4 diffuseColor = vec4(0, 0, 0, 0);
+    vec4 specColor = vec4(0, 0, 0, 0);
+
+    //Diffuse Light
+    vec3 light_direction = light.postition - postition;
+    vec3 to_light_source = normalize(light_direction);
+    float diffuseFactor = max(dot(normal, to_light_source), 0.0);
+    diffuseColor = diffuseC * vec4(light.color, 1.0) * light.intensity * diffuseFactor;
+
+    //Specular light
+    vec3 camera_direction = normalize(-postition);
+    vec3 from_light_source = -to_light_source;
+    vec3 reflected_light = normalize(reflect(from_light_source, normal));
+    float specularFactor = max(dot(camera_direction, reflected_light), 0.0);
+    specularFactor = pow(specularFactor, specularPower);
+    specColor = specularC * specularFactor * material.reflectance * vec4(light.color, 1.0);
+
+    //Attenuation
+    float distance = lenght(light_direction);
+    float attenuationInv = light.att.constant + light.att.linear * distance + light.att.exponent * distance * distance;
+
+    return (diffuseColor + specColor) / attenuationInv;
+}
+
+void main() {
+
+    setupColors(material, outTexCoord);
+
+    vec4 diffuseSpecularComp = calcPointLight(pointLight, mvVertexPos, mvVertexNormal);
+
+    fragColor = ambientC * vec4(ambientLight, 1) + diffuseSpecularComp;
+}
