56f069a0353234628ac70864b9605d9ee405d2ce
secknv <secknv@gmail.com>
2019-08-18 15:40:11 +0100

    finished TextItem class game item now moves with arrow keys

--- src/main/java/net/sknv/engine/graph/Texture.java
+++ src/main/java/net/sknv/engine/graph/Texture.java
@@ -11,27 +11,9 @@ import static org.lwjgl.stb.STBImage.*;
 
 public class Texture {
 
-    private final int id;
+    private final int id, width, height;
 
     public Texture(String filename) throws Exception{
-        this(loadTexture(filename));
-    }
-
-    public Texture(int id) {
-        this.id = id;
-    }
-
-    public void bind() {
-        glBindTexture(GL_TEXTURE_2D, id);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    private static int loadTexture(String filename) throws Exception {
-        int width, height;
-
         ByteBuffer buf;
 
         //load texture file
@@ -56,12 +38,29 @@ public class Texture {
         //each pixel in image has RGB and Alpha values. Tell openGL each value is 1byte
         glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
+        //Upload the texture data
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buf);
         glGenerateMipmap(GL_TEXTURE_2D);
 
         stbi_image_free(buf);
 
-        return textureId;
+        this.id = textureId;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void bind() {
+        glBindTexture(GL_TEXTURE_2D, id);
+    }
+
+    public int getId() {
+        return id;
     }
 
     public void cleanup() {
