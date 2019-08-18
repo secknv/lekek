c99e2e6898c1681127f83f57b6880b1debfb602b
dffsa <danyfreitas100@hotmail.com>
2019-08-18 15:41:58 +0100

    AABB calculation fixed, attempt at item AABB transformation

--- src/main/java/net/sknv/engine/BoundingBox.java
+++ src/main/java/net/sknv/engine/BoundingBox.java
@@ -1,6 +1,8 @@
 package net.sknv.engine;
 
+import org.joml.Matrix4f;
 import org.joml.Vector3f;
+import org.joml.Vector4f;
 
 public class BoundingBox {
     Vector3f min, max, rot;
@@ -26,5 +28,16 @@ public class BoundingBox {
 
     public Vector3f getRot() {
         return rot;
+    }
+
+    public void transform(Matrix4f modelViewMatrix) { //vec4 mvPos = modelViewMatrix * vec4(position, 1.0);
+        Vector4f min = new Vector4f(this.min.x, this.min.y, this.min.z, 1f);
+        Vector4f max = new Vector4f(this.max.x, this.max.y, this.max.z, 1f);
+
+        modelViewMatrix.transform(min);
+        modelViewMatrix.transform(max);
+
+        this.min = new Vector3f(min.x, min.y, min.z);
+        this.max = new Vector3f(max.x, max.y, max.z);
     }
 }
