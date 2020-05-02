a7402351512ae5e39d71bb0a416a3d1f8ee2a79f
Tiago Rosa <secknv@gmail.com>
2020-05-02 17:13:58 +0100

    Defined clean IGameLogic

--- src/main/java/net/sknv/engine/IGameLogic.java
+++ src/main/java/net/sknv/engine/IGameLogic.java
@@ -4,7 +4,7 @@ public interface IGameLogic {
 
     void init(Window window) throws Exception;
     void input(Window window, MouseInput mouseInput);
-    void update(Window window, float interval, MouseInput mouseInput);
-    void render(Window window, MouseInput mouseInput);
+    void update(float interval, MouseInput mouseInput);
+    void render(Window window);
     void cleanup();
 }
