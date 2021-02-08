533cc28378712313ef99cd72f6cde5171de47667
Tiago Rosa <tcrosa27@gmail.com>
2021-02-08 20:41:59 +0000

    Skybox changed to GameItemMesh subclass

--- src/main/java/net/sknv/engine/SkyBox.java
+++ src/main/java/net/sknv/engine/SkyBox.java
@@ -1,18 +1,20 @@
 package net.sknv.engine;
 
+import net.sknv.engine.entities.GameItemMesh;
 import net.sknv.engine.graph.Material;
 import net.sknv.engine.graph.Mesh;
 import net.sknv.engine.graph.OBJLoader;
 import net.sknv.engine.graph.Texture;
 
-public class SkyBox extends GameItem {
+public class SkyBox extends GameItemMesh {
 
-    public SkyBox(String objModel, String textureFile) throws Exception {//todo problem: SKYBOX NOT WORKING WHEN SERIALIZED
+    public SkyBox(String objModel, String textureFile) throws Exception {
+        //todo problem: SKYBOX NOT WORKING WHEN SERIALIZED
         super();
         Mesh skyBoxMesh = OBJLoader.loadMesh(objModel);
         Texture skyBoxTexture = new Texture(textureFile);
         skyBoxMesh.setMaterial(new Material(skyBoxTexture, 0));
         setMesh(skyBoxMesh);
-        setPos(0, 0, 0);
+        setPosition(0, 0, 0);
     }
 }
