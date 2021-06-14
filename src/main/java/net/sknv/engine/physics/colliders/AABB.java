1b14abfdcbd820aae56119a0928fc5aaa7ec4fee
dffsa <danyfreitas100@hotmail.com>
2021-06-14 01:03:05 +0100

    deleted commented deprecated procedure
    guess that is what VCS is for

--- src/main/java/net/sknv/engine/physics/colliders/AABB.java
+++ src/main/java/net/sknv/engine/physics/colliders/AABB.java
@@ -52,23 +52,6 @@ public class AABB implements BoundingBox {
         return max;
     }
 
-    /*
-    public void transform() {
-        Vector4f min = new Vector4f(this.min.getPosition().x, this.min.getPosition().y, this.min.getPosition().z, 1f);
-        Vector4f max = new Vector4f(this.max.getPosition().x, this.max.getPosition().y, this.max.getPosition().z, 1f);
-
-        //this view matrix ignores rotation so that we always get the same AABB for rotated items
-        Matrix4f modelViewMatrix = new Matrix4f();
-        modelViewMatrix.identity().translate(gameItem.getPos()).scale(gameItem.getScale());
-
-        modelViewMatrix.transform(min);
-        modelViewMatrix.transform(max);
-
-        this.min.setPosition(new Vector3f(min.x, min.y, min.z));
-        this.max.setPosition(new Vector3f(max.x, max.y, max.z));
-    }
-     */
-
     public void translate(Vector3f step){
         this.min.getPosition().add(step);
         this.max.getPosition().add(step);
